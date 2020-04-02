0119e74f88cd240a893596e64beb2c74eb853155
pravindeshmukh1 <pravin2391@gmail.com>
2020-04-02 19:23:08 +0530

    ADDED: 1.3- If CSV File Type Incorrect Return Custom Exception

--- src/test/java/CensusAnalyserTest.java
+++ src/test/java/CensusAnalyserTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 public class CensusAnalyserTest {
     private static String CSV_FILE_PATH = "src/test/resources/StateCensusData.csv";
     private static String WRONG_CSV_FILE_PATH = "src/test/resources/stateCensusData.csv";
+    private static String WRONG_CSV_FILE_TYPE = "src/test/resources/StateCensusData.txt";
 
     @Test
     public void givenStateCensusCSVFile_whenNumberOfRecordMatchesTrue_shouldReturnNumberOfRecordMatches() throws CensusAnalyserException {
@@ -19,6 +20,16 @@ public class CensusAnalyserTest {
         try {
             CensusAnalyser censusAnalyser = new CensusAnalyser();
             censusAnalyser.loadCsvData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.FILE_INCORRECT_EXCEPTION, e.exceptionType);
+        }
+    }
+
+    @Test
+    public void givenStateCensusCSVFile_whenFileTypeInCorrect_shouldReturnCustomException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            censusAnalyser.loadCsvData(WRONG_CSV_FILE_TYPE);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.FILE_INCORRECT_EXCEPTION, e.exceptionType);
         }
