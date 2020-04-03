c8aa938cdaa032dbe0b2bb49d7bdaa508b0d0ba8
pravindeshmukh1 <pravin2391@gmail.com>
2020-04-03 13:49:54 +0530

    REFACTOR 2 : Code to ensure Single Responsibility class and used Delegation Principle to handle CSV Files

--- src/main/java/com/analyser/services/CensusAnalyser.java
+++ src/main/java/com/analyser/services/CensusAnalyser.java
@@ -3,10 +3,7 @@ package com.analyser.services;
 import com.analyser.dao.StateCensusCsv;
 import com.analyser.dao.StateCodeCsv;
 import com.exception.CensusAnalyserException;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
 
-import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -25,7 +22,7 @@ public class CensusAnalyser {
 
     public int loadCsvData(String csvFilePath) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(CSV_FILE_PATH));) {
-            Iterator<StateCensusCsv> stateCensusCsvIterator = this.readCSVFileIterator(reader, StateCensusCsv.class);
+            Iterator<StateCensusCsv> stateCensusCsvIterator = new OpenCSVBuilder().readCSVFileIterator(reader, StateCensusCsv.class);
             Iterable<StateCensusCsv> stateCensusCsvIterable = () -> stateCensusCsvIterator;
             int noOfCount = 0;
             noOfCount = (int) StreamSupport.stream(stateCensusCsvIterable.spliterator(), false).count();
@@ -39,7 +36,7 @@ public class CensusAnalyser {
 
     public int loadStateCodeCsv(String csvStateCodePath) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(CSV_STATE_CODE_PATH));) {
-            Iterator<StateCodeCsv> stateCodeCsvIterator = this.readCSVFileIterator(reader, StateCodeCsv.class);
+            Iterator<StateCodeCsv> stateCodeCsvIterator = new OpenCSVBuilder().readCSVFileIterator(reader, StateCodeCsv.class);
             Iterable<StateCodeCsv> stateCodeCsvIterable = () -> stateCodeCsvIterator;
             int noOfCount = 0;
             noOfCount = (int) StreamSupport.stream(stateCodeCsvIterable.spliterator(), false).count();
@@ -49,20 +46,5 @@ public class CensusAnalyser {
         } catch (IOException e) {
             throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.FILE_INCORRECT_EXCEPTION, e.getMessage());
         }
-    }
-
-    public <E> Iterator<E> readCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException {
-        try {
-            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(csvClass);
-            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-            return csvToBean.iterator();
-        } catch (RuntimeException e) {
-            throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.FILE_HEADER_AND_DELIMITER_INCORRECT_EXCEPTION, e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
---
+++ src/main/java/com/analyser/services/OpenCSVBuilder.java
@@ -0,0 +1,25 @@
+package com.analyser.services;
+
+import com.exception.CensusAnalyserException;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.Reader;
+import java.util.Iterator;
+
+public class OpenCSVBuilder {
+    public <E> Iterator<E> readCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException {
+        try {
+            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(csvClass);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+            return csvToBean.iterator();
+        } catch (RuntimeException e) {
+            throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.FILE_HEADER_AND_DELIMITER_INCORRECT_EXCEPTION, e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
