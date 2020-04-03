196fee8fb9752b682b5f6741cb4037b677ef6153
pravindeshmukh1 <pravin2391@gmail.com>
2020-04-03 13:37:36 +0530

    REFACTOR: Code to Follow th DRY principle to extract data from csv file and count the records

--- src/main/java/com/analyser/services/CensusAnalyser.java
+++ src/main/java/com/analyser/services/CensusAnalyser.java
@@ -6,12 +6,14 @@ import com.exception.CensusAnalyserException;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
+import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
     private static final String CSV_FILE_PATH = "src/test/resources/StateCensusData.csv";
@@ -23,16 +25,10 @@ public class CensusAnalyser {
 
     public int loadCsvData(String csvFilePath) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(CSV_FILE_PATH));) {
-            CsvToBeanBuilder<StateCensusCsv> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(StateCensusCsv.class);
-            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<StateCensusCsv> csvToBean = csvToBeanBuilder.build();
-            Iterator<StateCensusCsv> stateCensusCsvIterator = csvToBean.iterator();
+            Iterator<StateCensusCsv> stateCensusCsvIterator = this.readCSVFileIterator(reader, StateCensusCsv.class);
+            Iterable<StateCensusCsv> stateCensusCsvIterable = () -> stateCensusCsvIterator;
             int noOfCount = 0;
-            while (stateCensusCsvIterator.hasNext()) {
-                noOfCount++;
-                StateCensusCsv stateCensusCsv = stateCensusCsvIterator.next();
-            }
+            noOfCount = (int) StreamSupport.stream(stateCensusCsvIterable.spliterator(), false).count();
             return noOfCount;
         } catch (IOException e) {
             throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.FILE_INCORRECT_EXCEPTION, e.getMessage());
@@ -43,20 +39,30 @@ public class CensusAnalyser {
 
     public int loadStateCodeCsv(String csvStateCodePath) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(CSV_STATE_CODE_PATH));) {
-            CsvToBeanBuilder<StateCodeCsv> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(StateCodeCsv.class).withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<StateCodeCsv> csvToBean = csvToBeanBuilder.build();
-            Iterator<StateCodeCsv> stateCodeCsvIterator = csvToBean.iterator();
+            Iterator<StateCodeCsv> stateCodeCsvIterator = this.readCSVFileIterator(reader, StateCodeCsv.class);
+            Iterable<StateCodeCsv> stateCodeCsvIterable = () -> stateCodeCsvIterator;
             int noOfCount = 0;
-            while (stateCodeCsvIterator.hasNext()) {
-                noOfCount++;
-                StateCodeCsv stateCodeCsv = stateCodeCsvIterator.next();
-            }
+            noOfCount = (int) StreamSupport.stream(stateCodeCsvIterable.spliterator(), false).count();
             return noOfCount;
         } catch (NoSuchFileException e) {
             throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.FILE_INCORRECT_EXCEPTION, e.getMessage());
         } catch (IOException e) {
             throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.FILE_INCORRECT_EXCEPTION, e.getMessage());
         }
+    }
+
+    public <E> Iterator<E> readCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException {
+        try {
+            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(csvClass);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+            return csvToBean.iterator();
+        } catch (RuntimeException e) {
+            throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.FILE_HEADER_AND_DELIMITER_INCORRECT_EXCEPTION, e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
